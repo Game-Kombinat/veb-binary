6259101e0681b2149404a04e8b036f292a8f0c45
Chris Ksoll <chris@gamekombinat.com>
2025-08-31 18:31:00 +0200

    Block based namespaces because Unitar

--- NullableSupport.cs
+++ NullableSupport.cs
@@ -1,27 +1,27 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace DataKombinat.Binary;
-
-public class NullableSupport {
-    /// <inheritdoc cref="Debug.Assert(bool)"/>
-    [Conditional("DEBUG")]
-    public static void Assert([DoesNotReturnIf(false)] bool b) => Debug.Assert(b);
-}
+namespace DataKombinat.Binary {
+    public class NullableSupport {
+        /// <inheritdoc cref="Debug.Assert(bool)"/>
+        [Conditional("DEBUG")]
+        public static void Assert([DoesNotReturnIf(false)] bool b) => Debug.Assert(b);
+    }
 
 // Copied from https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
 // and changed from public to internal.
-/// <summary>Specifies that the method will not return if the associated Boolean parameter is passed the specified value.</summary>
-[AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-internal sealed class DoesNotReturnIfAttribute : Attribute
-{
-    /// <summary>Initializes the attribute with the specified parameter value.</summary>
-    /// <param name="parameterValue">
-    /// The condition parameter value. Code after the method will be considered unreachable by diagnostics if the argument to
-    /// the associated parameter matches this value.
-    /// </param>
-    public DoesNotReturnIfAttribute(bool parameterValue) => ParameterValue = parameterValue;
+    /// <summary>Specifies that the method will not return if the associated Boolean parameter is passed the specified value.</summary>
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class DoesNotReturnIfAttribute : Attribute
+    {
+        /// <summary>Initializes the attribute with the specified parameter value.</summary>
+        /// <param name="parameterValue">
+        /// The condition parameter value. Code after the method will be considered unreachable by diagnostics if the argument to
+        /// the associated parameter matches this value.
+        /// </param>
+        public DoesNotReturnIfAttribute(bool parameterValue) => ParameterValue = parameterValue;
 
-    /// <summary>Gets the condition parameter value.</summary>
-    public bool ParameterValue { get; }
+        /// <summary>Gets the condition parameter value.</summary>
+        public bool ParameterValue { get; }
+    }
 }
